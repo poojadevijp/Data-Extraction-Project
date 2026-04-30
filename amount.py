d27ef2d5a7ad33a1f6b3eed7c042b80df3d400b2
poojadevijp <poojadevijp@gmail.com>
2026-04-30 10:09:42 +0530

    add python program

---
+++ amount.py
@@ -0,0 +1,16 @@
+amounts = []
+
+with open("bank_statement.txt", "r") as file:
+    lines = file.readlines()
+
+
+for line in lines[1:]:
+    parts = line.split("\t")   
+    amount = parts[3]          
+    amounts.append(float(amount))
+
+print("All transaction amounts:")
+print(amounts)
+
+print("\nTotal amount:")
+print(sum(amounts))
