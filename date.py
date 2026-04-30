d4db80731cc44e05865cc4b6a5c6f4ec4576eb64
krishnamoorthyvipi-cmyk <krishnamoorthyvipi@gmail.com>
2026-04-30 15:12:46 +0530

    updated file

---
+++ date.py
@@ -0,0 +1,9 @@
+dates = []
+
+with open("bank_statement.txt", "r") as file:
+    for line in file:
+        date = line.split()[1]
+        if date == "Date":
+            continue
+        dates.append(date)
+print(dates)
